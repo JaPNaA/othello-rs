0e142f4af0c0d5715692fcce40e0d334b6aeccd1
JaPNaA <japnaa@japnaa.dev>
2025-07-30 15:34:46 -0400

    Add Explain header IDs

--- htmlGen/generate_index.js
+++ htmlGen/generate_index.js
@@ -1,8 +1,11 @@
 import { readFileSync, writeFileSync } from "fs";
 import { marked } from "marked";
+import { gfmHeadingId } from "marked-gfm-heading-id";
 
 const template = readFileSync("index_template.html").toString();
 const readmeMd = readFileSync("../README.md").toString();
+
+marked.use(gfmHeadingId());
 
 const result = template.replace("{{readme-html}}", await marked(readmeMd));
 
